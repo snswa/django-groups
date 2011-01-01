c3e67df7ed636adf254c489c7b374cb38dd29147
Matthew Scott <gldnspud@gmail.com>
2011-01-01 13:32:44 -0800

    whitespace

--- groups/templatetags/group_tags.py
+++ groups/templatetags/group_tags.py
@@ -14,15 +14,15 @@ class GroupURLNode(template.Node):
         self.group = group
         self.kwargs = kwargs
         self.asvar = asvar
-    
+
     def render(self, context):
         url = ""
         group = self.group.resolve(context)
-        
+
         kwargs = {}
         for k, v in self.kwargs.items():
             kwargs[smart_str(k, "ascii")] = v.resolve(context)
-        
+
         if group:
             bridge = group.content_bridge
             try:
@@ -36,7 +36,7 @@ class GroupURLNode(template.Node):
             except NoReverseMatch:
                 if self.asvar is None:
                     raise
-                
+
         if self.asvar:
             context[self.asvar] = url
             return ""
@@ -53,7 +53,7 @@ class ContentObjectsNode(template.Node):
         else:
             self.gfk_field_var = None
         self.context_var = context_var
-    
+
     def render(self, context):
         group = self.group_var.resolve(context)
         model_name = self.model_name_var.resolve(context)
@@ -61,13 +61,13 @@ class ContentObjectsNode(template.Node):
             gfk_field = self.gfk_field_var.resolve(context)
         else:
             gfk_field = None
-        
+
         if isinstance(model_name, QuerySet):
             model = model_name
         else:
             app_name, model_name = model_name.split(".")
             model = get_model(app_name, model_name)
-        
+
         context[self.context_var] = group.content_objects(model, gfk_field=gfk_field)
         return ""
 
@@ -77,18 +77,18 @@ class ObjectGroupUrlNode(template.Node):
         self.obj_var = template.Variable(obj)
         self.group = group
         self.asvar = asvar
-    
+
     def render(self, context):
         url = ""
         obj = self.obj_var.resolve(context)
         group = self.group.resolve(context)
-        
+
         try:
             url = obj.get_absolute_url(group)
         except NoReverseMatch:
             if self.asvar is None:
                 raise
-        
+
         if self.asvar:
             context[self.asvar] = url
             return ""
@@ -103,13 +103,13 @@ def groupurl(parser, token):
     if len(bits) < 3:
         raise template.TemplateSyntaxError("'%s' takes at least two arguments"
             " (path to a view and a group)" % tag_name)
-    
+
     view_name = bits[1]
     group = parser.compile_filter(bits[2])
     args = []
     kwargs = {}
     asvar = None
-    
+
     if len(bits) > 3:
         bits = iter(bits[3:])
         for bit in bits:
@@ -124,7 +124,7 @@ def groupurl(parser, token):
                         kwargs[k] = parser.compile_filter(v)
                     elif arg:
                         raise template.TemplateSyntaxError("'%s' does not support non-kwargs arguments." % tag_name)
-    
+
     return GroupURLNode(view_name, group, kwargs, asvar)
 
 
@@ -132,12 +132,12 @@ def groupurl(parser, token):
 def content_objects(parser, token):
     """
     Basic usage::
-    
+
         {% content_objects group "tasks.Task" as tasks %}
-    
+
     or if you need to specify a custom generic foreign key field (default is
     group)::
-    
+
         {% content_objects group "tasks.Task" "content_object" as tasks %}
     """
     bits = token.split_contents()
@@ -155,7 +155,7 @@ def object_group_url(parser, token):
     """
     given an object and an optional group, call get_absolute_url passing the
     group variable::
-    
+
         {% object_group_url task group %}
     """
     bits = token.contents.split()
@@ -163,10 +163,10 @@ def object_group_url(parser, token):
     if len(bits) < 3:
         raise template.TemplateSyntaxError("'%s' takes at least two arguments"
             " (object and a group)" % tag_name)
-    
+
     obj = bits[1]
     group = parser.compile_filter(bits[2])
-    
+
     if len(bits) > 3:
         if bits[3] != "as":
             raise template.TemplateSyntaxError("'%s' requires the forth"
@@ -176,5 +176,5 @@ def object_group_url(parser, token):
         except IndexError:
             raise template.TemplateSyntaxError("'%s' requires an argument"
                 " after 'as'" % tag_name)
-    
+
     return ObjectGroupUrlNode(obj, group, asvar)
